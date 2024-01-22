0937b433d8fa238248dcb5ed124fdc1e82676bf8
unknown <aswathbabu2000@gmail.com>
2024-01-22 12:56:00 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -7,55 +7,71 @@ import { getMonths } from "./utils/getMonths";
 // import 'react-calendar/dist/Calendar.css'
 
 function App() {
+  const days = getDays();
+  const months = getMonths();
+  const [month, setMonth] = useState(1);
+  const [selectedDates, setSelectedDates] = useState([]);
 
-  const days=getDays();
-  const months=getMonths();
-  const [month,setMonth] = useState(1);
-  const [selectedDates,setSelectedDates] = useState([]);
+  const dates = month % 2 == 0 ? getThirtyDays() : getThirtyOneDays();
 
-  const dates=month%2==0?getThirtyDays():getThirtyOneDays();
-
-  const handlePrevClick=()=>{
-      if(month==1){
-        setMonth(12);
-      }else{
-        setMonth(month=>month-1);
-      }
-  }
-
-  const handleNextClick=()=>{
-    if(month==12){
-      setMonth(1);
-    }else{
-      setMonth(month=>month+1)
+  const handlePrevClick = () => {
+    if (month == 1) {
+      setMonth(12);
+    } else {
+      setMonth((month) => month - 1);
     }
-  }
+  };
 
-  return <div className="App">
+  const handleNextClick = () => {
+    if (month == 12) {
+      setMonth(1);
+    } else {
+      setMonth((month) => month + 1);
+    }
+  };
 
-  <div className="buttons">
-    <button onClick={handlePrevClick}>Prev</button>
-    <button onClick={handleNextClick}>Next</button>
-  </div>
+  return (
+    <div className="App">
+      <div className="buttons">
+        <button onClick={handlePrevClick}>Prev</button>
+        <button onClick={handleNextClick}>Next</button>
+      </div>
 
-  <div className="month">
-    <div className="days">
-      {days.map((day,i)=>{
-        return <p key={i} >{day}</p>
-      })}
+      <div className="month">
+        <div className="days">
+          {days.map((day, i) => {
+            return <p key={i}>{day}</p>;
+          })}
+        </div>
+        <div className="dates">
+          {dates.map((date, i) => (
+            <p
+              key={i}
+              onClick={(e) => {
+                e.target.classList.toggle("selected");
+                e.target.classList.contains("selected") &&
+                  setSelectedDates([...selectedDates, date]);
+              }}
+            >
+              {date}
+            </p>
+          ))}
+        </div>
+      </div>
+      <div className="selectedDates">
+        <h4>User Selected Dates:</h4>
+        {selectedDates.length > 0 &&
+          selectedDates.map((date, i) => {
+            return (
+              <p key={i}>
+                {date}-{month}-24
+              </p>
+            );
+          })}
+      </div>
+      {/* <Calendar/> */}
     </div>
-    <div className="dates">
-      {dates.map((date,i)=> <p key={i} onClick={(e)=>setSelectedDates([...selectedDates,date])}>{date}</p> )}
-    </div>
-  </div>
-  <div className="selectedDates">
-    <h4>User Selected Dates:</h4>
-      {selectedDates.length>0&&selectedDates.map((date,i)=>{
-        return <p key={i}>{date}-{month}-24</p>
-      })}
-  </div>
-{/* <Calendar/> */}
-  </div>;
+  );
 }
 
 export default App;

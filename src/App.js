a113fbed3f8b40b9a9c44cb4303d6d8dac2d46e3
unknown <aswathbabu2000@gmail.com>
2024-01-22 12:24:44 +0530

    Basic UI finished

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import { getDays } from "./utils/getDays";
+import { getThirtyDays, getThirtyOneDays } from "./utils/getDates";
+import Calendar from "react-calendar";
+import 'react-calendar/dist/Calendar.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  // const days=getDays();
+  // const [month,setMonth] = useState(1);
+  // const [selectedDates,setSelectedDates] = useState([]);
+
+  // const dates=month%2==0?getThirtyDays():getThirtyOneDays();
+
+  return <div className="App">
+
+  {/* <div className="buttons">
+    <button onClick={()=>setMonth(month=>month-1)}>Prev</button>
+    <button onClick={()=>setMonth(month=>month+1)}>Next</button>
+  </div>
+
+  <div className="month">
+    <div className="days">
+      {days.map((day,i)=>{
+        return <p key={i}>{day}</p>
+      })}
     </div>
-  );
+    <div className="dates">
+      {dates.map((date,i)=> <p key={i}>{date}</p> )}
+    </div>
+  </div> */}
+<Calendar/>
+  </div>;
 }
 
 export default App;
---
+++ src/utils/getDays.js
@@ -0,0 +1,1 @@
+export const getDays = () => ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
